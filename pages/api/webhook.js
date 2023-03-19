6f26242eb2101bbb48451c6c31eb861abb3b7022
CodeMat <codemat.biz@gmail.com>
2023-03-19 14:38:08 +0100

    updated

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -1,66 +1,16 @@
-import { Buffer } from "buffer";
-import crypto from "crypto";
+import { verifyPaystackEvent } from 'paystack'
 
-const paystackSecretKey = "sk_test_8af66b83b1a31986ff06a8c48ea7558c6119963e";
+export default async function handlePaystackEvents(req, res) {
+  const event = req.body;
+  const secretKey = "sk_test_8af66b83b1a31986ff06a8c48ea7558c6119963e";
+  const isValid = verifyPaystackEvent(event, secretKey);
 
-function verifyPaystackSignature(signature, body) {
-  const hash = crypto
-    .createHmac("sha512", paystackSecretKey)
-    .update(body)
-    .digest("hex");
-  return signature === hash;
-}
-
-async function handlePaystackEvent(req, res) {
-  if (req.method !== "POST") {
-    res.statusCode = 405;
-    res.end("Method not allowed");
-    return;
-  }
-
-  const signature = req.headers["x-paystack-signature"];
-  const body =
-    (await new Promise()) <
-    Buffer >
-    ((resolve) => {
-      const chunks = [];
-      req.on("data", (chunk) => chunks.push(chunk));
-      req.on("end", () => resolve(Buffer.concat(chunks)));
-    });
-
-  if (!verifyPaystackSignature(signature, body)) {
-    res.statusCode = 400;
-    res.end("Invalid signature");
-    return;
-  }
-
-  const event = req.headers["x-paystack-event"];
-  const eventData = JSON.parse(body.toString());
-
-  // Handle Paystack events
-  switch (event) {
-    case "charge.success":
-      // Do something when a charge is successful
-      break;
-    case "charge.failure":
-      // Do something when a charge fails
-      break;
-    // Handle other Paystack events here
-    default:
-      // Unknown event type
-      break;
-  }
-
-  res.statusCode = 200;
-  res.end("OK");
-}
-
-export default async function webhook(req, res) {
-  try {
-    await handlePaystackEvent(req, res);
-  } catch (err) {
-    console.error(err);
-    res.statusCode = 500;
-    res.end("Internal Server Error");
-  }
+    if (isValid) {
+      console.log("Received a valid Paystack event:", event);
+      // Do something with the event, e.g. update your database, send a notification, etc.
+      res.status(200).end();
+    } else {
+      console.log("Received an invalid Paystack event:", event);
+      res.status(400).end();
+    }
 }
