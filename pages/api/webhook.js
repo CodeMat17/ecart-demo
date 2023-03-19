73eef77f4531d5b1d165af202a9b9627f0c9faa2
CodeMat <codemat.biz@gmail.com>
2023-03-20 00:46:49 +0100

    added

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -1,27 +1,33 @@
-import bodyParser from "body-parser";
+import { verify } from "crypto";
 
-// create middleware function to parse request body
-const parseBody = bodyParser.json();
-
-// create route to handle Paystack webhook events
-export default async function handler(
+export default async function handlePaystackWebhook(
   req,
   res
 ) {
-  if (req.method === "POST") {
-    // use parseBody middleware to parse request body
-    parseBody(req, res, () => {
-      // access Paystack webhook event data
-      const event = req.body;
-      console.log(event);
-
-      // handle Paystack webhook event
-      // ...
-
-      // send response
-      res.status(200).end();
-    });
-  } else {
-    res.status(405).end();
+  if (req.method !== "POST") {
+    res.status(405).json({ error: "Method not allowed" });
+    return;
   }
+
+  const secret = "sk_test_8af66b83b1a31986ff06a8c48ea7558c6119963e";
+  const hash = req.headers["x-paystack-signature"];
+
+  if (!secret || !hash) {
+    res.status(400).json({ error: "Missing webhook secret or signature" });
+    return;
+  }
+
+  const hmac = verify(req.rawBody, secret, hash);
+
+  if (!hmac) {
+    res.status(401).json({ error: "Invalid signature" });
+    return;
+  }
+
+  // Handle the Paystack webhook event here
+  const data = req.body;
+
+  console.log("Received Paystack webhook event:", data);
+
+  res.status(200).json({ message: "Webhook received successfully" });
 }
