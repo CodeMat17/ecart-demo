d23062facad5dfc655743c1e13900000835f28ea
CodeMat <codemat.biz@gmail.com>
2023-03-19 03:30:14 +0100

    updated

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -2,8 +2,11 @@
 
 export default function handler(req, res) {
   const events = req.body;
-  console.log("events", events);
-  console.log("secret", events.secret);
+  if (secret === "sk_test_8af66b83b1a31986ff06a8c48ea7558c6119963e") {
+    res.status(200).json({ message: "successful" }, {events: {events}});
+  }
+//   console.log("events", events);
+//   console.log("secret", events.secret);
 
-  res.status(200).json({ name: "John Doe" });
+  //   res.status(200).json({ name: "John Doe" });
 }
