57956b50075b1f80ee6a6c3317d3f3dd77736cb0
CodeMat <codemat.biz@gmail.com>
2023-03-19 02:30:30 +0100

    first commit

---
+++ pages/api/webhook.js
@@ -0,0 +1,9 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  const events = req.body;
+  console.log("events", events);
+//   console.log("secret", secret);
+
+  res.status(200).json({ name: "John Doe" });
+}
