27fc865fea965578997c37dde334ccd117960b3f
CodeMat <codemat.biz@gmail.com>
2023-03-19 14:02:51 +0100

    updated

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -1,11 +1,66 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { Buffer } from "buffer";
+import crypto from "crypto";
 
-export default function handler(req, res) {
-  if (req.method === "POST") {
-    const event = req.body;
-    console.log(event);
-    res.status(200).json({ message: "successful" });
-  } else {
-    res.status(400).json({ message: "Invalid request method" });
+const paystackSecretKey = process.env.PAYSTACK_SECRET_KEY;
+
+function verifyPaystackSignature(signature, body) {
+  const hash = crypto
+    .createHmac("sha512", paystackSecretKey)
+    .update(body)
+    .digest("hex");
+  return signature === hash;
+}
+
+async function handlePaystackEvent(req, res) {
+  if (req.method !== "POST") {
+    res.statusCode = 405;
+    res.end("Method not allowed");
+    return;
+  }
+
+  const signature = req.headers["x-paystack-signature"];
+  const body =
+    (await new Promise()) <
+    Buffer >
+    ((resolve) => {
+      const chunks = [];
+      req.on("data", (chunk) => chunks.push(chunk));
+      req.on("end", () => resolve(Buffer.concat(chunks)));
+    });
+
+  if (!verifyPaystackSignature(signature, body)) {
+    res.statusCode = 400;
+    res.end("Invalid signature");
+    return;
+  }
+
+  const event = req.headers["x-paystack-event"];
+  const eventData = JSON.parse(body.toString());
+
+  // Handle Paystack events
+  switch (event) {
+    case "charge.success":
+      // Do something when a charge is successful
+      break;
+    case "charge.failure":
+      // Do something when a charge fails
+      break;
+    // Handle other Paystack events here
+    default:
+      // Unknown event type
+      break;
+  }
+
+  res.statusCode = 200;
+  res.end("OK");
+}
+
+export default async function webhook(req, res) {
+  try {
+    await handlePaystackEvent(req, res);
+  } catch (err) {
+    console.error(err);
+    res.statusCode = 500;
+    res.end("Internal Server Error");
   }
 }
