d29dcb63e3affe252005207de94889778fdca94c
CodeMat <codemat.biz@gmail.com>
2023-03-21 00:15:56 +0100

    updated

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -1,12 +1,12 @@
 export default function handler(req, res) {
-  if (req.method === "POST") {
-    const event = req.body;
-
-    console.log("Paystack webhook event received:", event);
-
-    res.status(200).json({ message: "Webhook received successfully" });
-  } else {
+  if (req.method !== "POST") {
     res.setHeader("Allow", ["POST"]);
     res.status(405).json({ message: `Method ${req.method} not allowed` });
   }
+
+  const event = req.body;
+
+  console.log("Paystack webhook event received:", event);
+
+  res.status(200).json({ message: "Webhook received successfully" });
 }
