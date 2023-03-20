916ac3dc863699b5f09f85f0b806ebd4e5c58d56
CodeMat <codemat.biz@gmail.com>
2023-03-20 01:49:12 +0100

    updated

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -1,10 +1,8 @@
 export default async function handlePaystackWebhook(req, res) {
   // Handle the Paystack webhook event here
   const data = req.body;
-  if (data) {
+
     console.log("Received Paystack webhook event:", data);
     res.status(200).json({ message: "Webhook received successfully" });
-  } else {
-    res.status(400).json({ error: "Not responding" });
-  }
+
 }
